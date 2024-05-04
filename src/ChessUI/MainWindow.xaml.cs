05f8281da1973d6d130940460cc034567fae8cc4
kauatwn <kaualopescontato@gmail.com>
2024-05-04 18:19:49 -0300

    Inicializando tabuleiro e estado do jogo na MainWindow

--- src/ChessUI/MainWindow.xaml.cs
+++ src/ChessUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ChessLogic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,42 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image[,] PieceImages { get; } = new Image[8, 8];
+        private GameState GameState { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            GameState = new GameState(Board.Initial(), Player.White);
+            DrawBoard(GameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    var image = new Image();
+                    PieceImages[row, column] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    Piece piece = board[row, column];
+
+                    PieceImages[row, column].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
