71f32c546d240c142bf16d498b94d649560268c1
kauatwn <kaualopescontato@gmail.com>
2024-05-05 16:20:57 -0300

    Criando métodos na classe base para encontrar posições

--- src/ChessLogic/Pieces/Piece.cs
+++ src/ChessLogic/Pieces/Piece.cs
@@ -10,5 +10,31 @@ namespace ChessLogic
         public abstract Piece Copy();
 
         public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+
+        protected IEnumerable<Position> FindPositionsInUniqueDirection(Position from, Board board, Direction direction)
+        {
+            for (Position position = from + direction; Board.IsInside(position); position += direction)
+            {
+                if (board.IsEmpty(position))
+                {
+                    yield return position;
+                    continue;
+                }
+
+                Piece piece = board[position];
+
+                if (piece.Color != Color)
+                {
+                    yield return position;
+                }
+
+                yield break;
+            }
+        }
+
+        protected IEnumerable<Position> FindPositionsInMultipleDirections(Position from, Board board, Direction[] directions)
+        {
+            return directions.SelectMany(direction => FindPositionsInUniqueDirection(from, board, direction));
+        }
     }
 }
