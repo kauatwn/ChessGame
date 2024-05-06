c5563b85b3afb950618b5d97b323d776d3d05655
kauatwn <kaualopescontato@gmail.com>
2024-05-06 10:47:49 -0300

    Criando métodos para encontrar movimentos para King

--- src/ChessLogic/Pieces/King.cs
+++ src/ChessLogic/Pieces/King.cs
@@ -31,5 +31,31 @@
 
             return copy;
         }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction direction in Directions)
+            {
+                Position to = from + direction;
+
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
     }
 }
