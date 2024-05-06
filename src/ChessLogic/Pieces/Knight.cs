13e7af88c089f30fc590766d9c35aa89789671c3
kauatwn <kaualopescontato@gmail.com>
2024-05-06 10:33:20 -0300

    Criando métodos para encontrar movimentos para Knight

--- src/ChessLogic/Pieces/Knight.cs
+++ src/ChessLogic/Pieces/Knight.cs
@@ -19,5 +19,28 @@
 
             return copy;
         }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return JumpMoves(from, board).Select(to => new NormalMove(from, to));
+        }
+
+        private static IEnumerable<Position> GetPossibleJumpMoves(Position from)
+        {
+            foreach (Direction horizontalDirection in new Direction[] { Direction.West, Direction.East })
+            {
+                foreach (Direction verticalDirection in new Direction[] { Direction.North, Direction.South })
+                {
+                    yield return from + 2 * horizontalDirection + verticalDirection;
+
+                    yield return from + 2 * verticalDirection + horizontalDirection;
+                }
+            }
+        }
+
+        private IEnumerable<Position> JumpMoves(Position from, Board board)
+        {
+            return GetPossibleJumpMoves(from).Where(position => Board.IsInside(position) && (board.IsEmpty(position) || board[position].Color != Color));
+        }
     }
 }
