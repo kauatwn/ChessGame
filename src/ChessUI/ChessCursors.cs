567ae86e72c3d47f1238d291f7caab2721b99023
kauatwn <kaualopescontato@gmail.com>
2024-05-10 13:51:44 -0300

    Criando ChessCursors para carregar cursores personalizados

---
+++ src/ChessUI/ChessCursors.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    public static class ChessCursors
+    {
+        public static Cursor WhiteCursor { get; } = LoadCursor("Assets/CursorW.cur");
+        public static Cursor BlackCursor { get; } = LoadCursor("Assets/CursorB.cur");
+
+        private static Cursor LoadCursor(string filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
+
+            return new Cursor(stream, true);
+        }
+    }
+}
