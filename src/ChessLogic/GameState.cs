b0243aca98135031fbb5d0a8f9a260bd5e4e91d5
kauatwn <kaualopescontato@gmail.com>
2024-05-12 19:28:36 -0300

    Criando métodos para legalidade dos movimentos, detecção de fim de jogo e determinação do resultado

--- src/ChessLogic/GameState.cs
+++ src/ChessLogic/GameState.cs
@@ -4,6 +4,7 @@
     {
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
+        public Result Result { get; private set; } = null;
 
         public GameState(Board board, Player player)
         {
@@ -30,6 +31,40 @@
             move.Execute(Board);
 
             CurrentPlayer = CurrentPlayer.Opponent();
+
+            CheckForGameOver();
+        }
+
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(position =>
+            {
+                Piece piece = Board[position];
+
+                return piece.GetMoves(position, Board);
+            });
+
+            return moveCandidates.Where(move => move.IsLegal(Board));
+        }
+
+        public bool IsGameOVer()
+        {
+            return Result != null;
+        }
+
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
         }
     }
 }
