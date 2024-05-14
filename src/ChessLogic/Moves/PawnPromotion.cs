57e8222ce1956abad8413cbe981c82e35ad7ac02
kauatwn <kaualopescontato@gmail.com>
2024-05-14 19:50:07 -0300

    Criando classe PawnPromotion para representar jogadas de promoção de peão

---
+++ src/ChessLogic/Moves/PawnPromotion.cs
@@ -0,0 +1,40 @@
+﻿namespace ChessLogic
+{
+    public class PawnPromotion : Move
+    {
+        public override MoveType Type => MoveType.PawnPromotion;
+        public override Position FromPosition { get; }
+        public override Position ToPosition { get; }
+
+        private PieceType NewType { get; }
+
+        public PawnPromotion(Position from, Position to, PieceType newType)
+        {
+            FromPosition = from;
+            ToPosition = to;
+            NewType = newType;
+        }
+
+        public override void Execute(Board board)
+        {
+            Piece pawn = board[FromPosition];
+            board[FromPosition] = null;
+
+            Piece promotionPiece = CreatePromotionPiece(pawn.Color);
+            promotionPiece.HasMoved = true;
+
+            board[ToPosition] = promotionPiece;
+        }
+
+        private Piece CreatePromotionPiece(Player color)
+        {
+            return NewType switch
+            {
+                PieceType.Knight => new Knight(color),
+                PieceType.Bishop => new Bishop(color),
+                PieceType.Rook => new Rook(color),
+                _ => new Queen(color),
+            };
+        }
+    }
+}
