14f3b2c384c75353de8eafbec4f65834681e85c7
kauatwn <kaualopescontato@gmail.com>
2024-05-21 18:44:08 -0300

    Criando classe EnPassant para representar o movimento de captura 'Ao Passar'

---
+++ src/ChessLogic/Moves/EnPassant.cs
@@ -0,0 +1,24 @@
+﻿namespace ChessLogic
+{
+    public class EnPassant : Move
+    {
+        public override MoveType Type => MoveType.EnPassant;
+        public override Position FromPosition { get; }
+        public override Position ToPosition { get; }
+
+        private Position CapturePosition { get; }
+
+        public EnPassant(Position from, Position to)
+        {
+            FromPosition = from;
+            ToPosition = to;
+            CapturePosition = new Position(from.Row, to.Column);
+        }
+
+        public override void Execute(Board board)
+        {
+            new NormalMove(FromPosition, ToPosition).Execute(board);
+            board[CapturePosition] = null;
+        }
+    }
+}
