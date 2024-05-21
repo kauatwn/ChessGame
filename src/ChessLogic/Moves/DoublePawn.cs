bb71a3896b2b6ec53ae11502293a24b6cc297b71
kauatwn <kaualopescontato@gmail.com>
2024-05-21 18:20:51 -0300

    Criando classe DoublePawn para tratamento específico do movimento duplo do peão

---
+++ src/ChessLogic/Moves/DoublePawn.cs
@@ -0,0 +1,26 @@
+﻿namespace ChessLogic
+{
+    public class DoublePawn : Move
+    {
+        public override MoveType Type => MoveType.DoublePawn;
+        public override Position FromPosition { get; }
+        public override Position ToPosition { get; }
+
+        private Position SkippedPosition { get; }
+
+        public DoublePawn(Position from, Position to)
+        {
+            FromPosition = from;
+            ToPosition = to;
+            SkippedPosition = new Position((from.Row + to.Row) / 2, from.Column);
+        }
+
+        public override void Execute(Board board)
+        {
+            Player player = board[FromPosition].Color;
+            board.SetPawnSkipPosition(player, SkippedPosition);
+
+            new NormalMove(FromPosition, ToPosition).Execute(board);
+        }
+    }
+}
