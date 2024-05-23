7210df70c4533f0420c1b7aee6e1d57e3945f186
kauatwn <kaualopescontato@gmail.com>
2024-05-23 10:44:59 -0300

    Criando classe Counting para rastrear movimentos das peças de xadrez

---
+++ src/ChessLogic/Counting.cs
@@ -0,0 +1,43 @@
+﻿namespace ChessLogic
+{
+    public class Counting
+    {
+        private Dictionary<PieceType, int> WhiteCount { get; } = [];
+        private Dictionary<PieceType, int> BlackCount { get; } = [];
+
+        public int TotalCount { get; private set; }
+
+        public Counting()
+        {
+            foreach (PieceType type in Enum.GetValues(typeof(PieceType)))
+            {
+                WhiteCount[type] = 0;
+                BlackCount[type] = 0;
+            }
+        }
+
+        public void Increment(Player color, PieceType type)
+        {
+            if (color == Player.White)
+            {
+                WhiteCount[type]++;
+            }
+            else if (color == Player.Black)
+            {
+                BlackCount[type]++;
+            }
+
+            TotalCount++;
+        }
+
+        public int White(PieceType type)
+        {
+            return WhiteCount[type];
+        }
+
+        public int Black(PieceType type)
+        {
+            return BlackCount[type];
+        }
+    }
+}
